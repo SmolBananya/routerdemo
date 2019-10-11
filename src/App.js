4deaa8d6d207b0a9ccc64223b2acd346c18261f2
Ronja Kaskela <ronja.kaskela@gmail.com>
2019-10-11 08:43:43 +0300

    finished routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function App() {
+const Home = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>This is Home page</h1>
     </div>
   );
-}
+};
+
+const About = () => {
+  return (
+    <div>
+      <h1>This is About page</h1>
+    </div>
+  );
+};
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>This is Contact page</h1>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="App">
+      <Router>
+          <nav>
+            <ul>
+              <li>
+              <Link to="/">Home</Link>
+              </li>
+              <li>
+              <Link to="/about">About</Link>
+              </li>
+              <li>
+              <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+          </nav>
+        <Switch>
+          <Route exact path="/" component={Home} />
+
+          <Route path="/about" component={About}/>
+
+          <Route path="/contact" component={Contact}>
+            <Contact />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
